475545ce59a1806fa05691f6c76f650bb0682866
shiyuwudi <289052669@qq.com>
2016-04-30 11:18:26 +0800

    add markdown syntax to children of comment div

--- public/src/CommentBox.js
+++ public/src/CommentBox.js
@@ -1,10 +1,31 @@
-//tutorial1.js
+
+
+var Comment = React.createClass({
+
+  rawMarkup: function() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize:true});
+    return {__html: rawMarkup};
+  }
+
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {/*children就是子节点*/}
+        <span dangerouslySetInnerHTML={this.rawMarkup()}></span>
+      </div>
+    );
+  }
+});
 
 var CommentList = React.createClass({
   render: function() {
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        <Comment author="shiyuwudi"> enjoy a unique three-day holiday!</Comment>
+        <Comment author="floor one's mother">enjoy *your~* sister!</Comment>
       </div>
     );
   }
